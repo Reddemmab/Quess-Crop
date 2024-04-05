48775cbe085d6f7d262fec58edff22ca762902e0
Reddemmab <162442837+Reddemmab@users.noreply.github.com>
2024-04-05 14:43:31 +0530

    Update urls.py

--- Telecom Proj/Telecom/CDR/urls.py
+++ Telecom Proj/Telecom/CDR/urls.py
@@ -3,7 +3,7 @@ from .views import UserDataByName, load_cdr_data, load_user_data, UserDataByName
 
 urlpatterns = [
     path('load_cdr_data/', load_cdr_data, name='load_cdr_data'),
-    path('load_cdr_data/', load_user_data, name='load_cdr_data'),
+    path('load_user_data/', load_user_data, name='load_user_data'),
     path('user_data/<str:user_name>/', UserDataByName.as_view(), name='user_data_by_name'),
     path('user_data/<str:user_name>/<str:date>/', UserDataByNameAndDate.as_view(), name='user_data_by_name_and_date'),
     path('user_data/<str:user_name>/<str:from_date>/<str:to_date>/', UserDataByDateRange.as_view(), name='user_data_by_date_range'),
